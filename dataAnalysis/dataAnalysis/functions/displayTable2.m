e2793f8607f2e1af396aa02858a83040916f2e08
francescacavallo <francescar.cavallo@libero.it>
2018-06-19 12:45:52 +0100

    Delete displayTable2.m

--- dataAnalysis/dataAnalysis/functions/displayTable2.m
+++
@@ -1,9 +0,0 @@
-
-function table = displayTable2(array, type)
-table = array2table(array, ...
-    'VariableNames',{'Leptin','BMI','Calories','GenotypeID'},...
-    'RowNames',{'Leptin','BMI','Calories','GenotypeID'});
-name = [num2str(type), ' Coefficients'];
-disp(name)
-disp(table)
-end
