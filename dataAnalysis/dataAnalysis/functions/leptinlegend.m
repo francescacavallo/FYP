99ef51d33f2a4b5104243892ea6e380e7ab848ae
francescacavallo <francescar.cavallo@libero.it>
2018-06-19 12:46:44 +0100

    Delete leptinlegend.m

--- dataAnalysis/dataAnalysis/functions/leptinlegend.m
+++
@@ -1,6 +0,0 @@
-function leptinlegend
-ylabel('Salivary leptin (pg/ml)')
-names = {'Week 0'; 'Week 2'; 'Week 4'; 'Week 6'; 'Week 8'};
-set(gca,'xtick',[1:5],'xticklabel',names)
-set(gca,'fontsize',20)
-end
