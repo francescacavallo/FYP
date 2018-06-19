c278652a1b0e0473c00443d60413e99b384482c6
francescacavallo <francescar.cavallo@libero.it>
2018-06-19 12:46:49 +0100

    Delete plotMean.m

--- dataAnalysis/dataAnalysis/functions/plotMean.m
+++
@@ -1,8 +0,0 @@
-function plotMean(mean,CI)
-    figure
-    hold all
-    plot(mean,'lineWidth',2) 
-    plot(CI', 'r--','lineWidth',2)
-    leptinlegend
-    legend('Mean Leptin', '95% Confidence Intervals')
-end
